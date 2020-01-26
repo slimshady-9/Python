1421e290ddcafd4e1209bd3dd1929cd162499b60
Ketan Sarda <50589540+slimshady-9@users.noreply.github.com>
2020-01-26 18:18:53 +0530

    Using MATPLOTLIB

--- Mplot.py
+++ Mplot.py
@@ -1,13 +1,13 @@
-import pandas as pd
+import pandas as pd;
 import matplotlib.pyplot as plt
 
 excel_file='C:\Programming\Python\Python\ML\Marvellous.xlsx';
 data=pd.read_excel(excel_file);
 
-data['Age'].plot(kind="hist")
+data['Age'].plot(kind="hist");
 plt.show();
 
-data['Age'].plot(kind="barh")
+data['Age'].plot(kind="barh");
 plt.show();
 
 sorted_data=data.sort_values(["Name"]);
